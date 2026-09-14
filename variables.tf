132273aae71cf23689ee58c150bb07ff0d6f16a5
Lutendo <luthoeec025@student.wethinkcode.co.za>
2026-09-14 15:19:46 +0200

    terraform input variables

---
+++ variables.tf
@@ -0,0 +1,16 @@
+﻿variable "instance_type" {
+  description = "EC2 instance type"
+  type = string 
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Name of an existing EC2 key for SSH access"
+  type = string
+}
+
+varible "allowed_ssh_cdir" {
+  description = "CIDR block allowed to SSH into the instance"
+  type = string
+  default = "0.0.0.0/0"
+}
