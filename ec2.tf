9acaae149b1f0ac84c57a42f60cb181fe255e377
Lutendo <luthoeec025@student.wethinkcode.co.za>
2026-09-15 22:22:39 +0200

    Add security group with SSH access

--- ec2.tf
+++ ec2.tf
@@ -7,3 +7,22 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
+resource "aws_security_group" "app_sg" {
+  name        = "flask-app-sg"
+  description = "Allow SSH and app traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
