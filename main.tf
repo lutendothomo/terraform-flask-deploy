38f7d26ce10088ae17d956f8ac6d63fa9fd1523b
Lutendo <luthoeec025@student.wethinkcode.co.za>
2026-09-24 11:02:40 +0200

    add s3 remote backend for terraform state

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket = "lutendothomo-tfstate-flaskdeploy"
+    key    = "flask-deploy/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
